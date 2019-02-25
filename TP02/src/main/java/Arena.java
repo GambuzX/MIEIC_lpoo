972f86c556fff6496042022abff249cb3f054235
GambuzX <mg_mesquita@hotmail.com>
2019-02-25 15:53:17 +0000

    Added walls to the arena

--- TP02/src/main/java/Arena.java
+++ TP02/src/main/java/Arena.java
@@ -5,28 +5,53 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Arena {
 
     private int width;
     private int height;
 
     private Hero hero;
+    private List<Wall> walls;
 
     public Arena(int w, int h) {
         this.width = w;
         this.height = h;
 
         this.hero = new Hero(10, 10);
+        this.walls = createWalls();
     }
 
     public void draw(TextGraphics graphics) {
         graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
         graphics.fillRectangle(new TerminalPosition(0,0), new TerminalSize(width, height), ' ');
         hero.draw(graphics);
+
+        for (Wall wall : walls)
+            wall.draw(graphics);
+    }
+
+    private List<Wall> createWalls() {
+
+        List<Wall> walls = new ArrayList<>();
+
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height-1));
+        }
+
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+
+        return walls;
     }
 
     private boolean canHeroMove(Position position) {
-        if (position.getX() >= 0 && position.getX() < width && position.getY() >= 0 && position.getY() < height)
+        if (position.getX() > 0 && position.getX() < width-1 && position.getY() > 0 && position.getY() < height-1)
             return true;
         return false;
     }
