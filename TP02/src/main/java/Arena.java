7a5afe836a703542addf19361ff728cedf6fe008
GambuzX <mg_mesquita@hotmail.com>
2019-02-25 16:26:53 +0000

    Implemented coins

--- TP02/src/main/java/Arena.java
+++ TP02/src/main/java/Arena.java
@@ -7,6 +7,7 @@ import com.googlecode.lanterna.screen.Screen;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Arena {
 
@@ -15,6 +16,7 @@ public class Arena {
 
     private Hero hero;
     private List<Wall> walls;
+    private List<Coin> coins;
 
     public Arena(int w, int h) {
         this.width = w;
@@ -22,6 +24,7 @@ public class Arena {
 
         this.hero = new Hero(10, 10);
         this.walls = createWalls();
+        this.coins = createCoins();
     }
 
     public void draw(TextGraphics graphics) {
@@ -31,6 +34,9 @@ public class Arena {
 
         for (Wall wall : walls)
             wall.draw(graphics);
+
+        for (Coin coin : coins)
+            coin.draw(graphics);
     }
 
     private List<Wall> createWalls() {
@@ -50,6 +56,22 @@ public class Arena {
         return walls;
     }
 
+    private List<Coin> createCoins() {
+        Random random = new Random();
+        ArrayList<Coin> coins = new ArrayList<>();
+
+        int added = 0;
+        while (added < 5) {
+            Position new_position = new Position ( random.nextInt(width - 2) + 1, random.nextInt(height - 2) + 1);
+
+            if (!coinAtPosition(new_position)) {
+                coins.add(new Coin(new_position.getX(), new_position.getY()));
+                added++;
+            }
+        }
+        return coins;
+    }
+
     private boolean canHeroMove(Position position) {
         boolean can_move = false;
         if (position.getX() >= 0 && position.getX() < width && position.getY() >= 0 && position.getY() < height) {
@@ -61,13 +83,33 @@ public class Arena {
                 return false;
             }
         }
-
         return can_move;
     }
 
+    private boolean coinAtPosition(Position position) {
+        if (coins == null)
+            return false;
+
+        for(Coin coin : coins)
+            if (coin.getPosition().equals(position))
+                return true;
+        return false;
+    }
+
     private void moveHero(Position position) {
-        if (canHeroMove(position))
+        if (canHeroMove(position)) {
             this.hero.setPosition(position);
+            retrieveCoins(position);
+        }
+    }
+
+    private void retrieveCoins(Position position) {
+        for (Coin coin : coins) {
+            if (coin.getPosition().equals(position)) {
+                coins.remove(coin);
+                break;
+            }
+        }
     }
 
     public void processKey(KeyStroke key) {
--- TP02/src/main/java/Coin.java
+++ TP02/src/main/java/Coin.java
@@ -3,17 +3,15 @@ import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
-public class Wall extends Element {
+public class Coin extends Element {
 
-
-    public Wall(int x, int y) {
+    public Coin(int x, int y) {
         super(x,y);
     }
 
     public void draw(TextGraphics graphics) {
         graphics.setForegroundColor(TextColor.Factory.fromString("#FFFF33"));
         graphics.enableModifiers(SGR.BOLD);
-        graphics.putString(new TerminalPosition(position.getX(), position.getY()), "#");
+        graphics.putString(new TerminalPosition(position.getX(), position.getY()), "O");
     }
-
 }
