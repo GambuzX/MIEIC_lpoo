a306847ce95b0516fabf66d2d69ed3cfe89db51d
GambuzX <mg_mesquita@hotmail.com>
2019-02-25 15:08:13 +0000

    Moving symbol with keystrokes

--- TP02/src/main/java/Game.java
+++ TP02/src/main/java/Game.java
@@ -1,14 +1,19 @@
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.input.KeyStroke;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Game {
 
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
 
     public Game() {
         try {
@@ -26,13 +31,43 @@ public class Game {
 
     private void draw() throws IOException {
         this.screen.clear();
-        this.screen.setCharacter(10, 10, new TextCharacter('X'));
+        this.screen.setCharacter(x, y, new TextCharacter('X'));
         this.screen.refresh();
+    }
+
+    private void processKey(KeyStroke key) {
+        switch (key.getKeyType()) {
+            case ArrowUp:
+                y -= 1;
+                break;
+            case ArrowDown:
+                y += 1;
+                break;
+            case ArrowLeft:
+                x -= 1;
+                break;
+            case ArrowRight:
+                x += 1;
+                break;
+            default:
+                System.out.println(key);
+                break;
+        }
     }
 
     public void run() {
         try {
-            draw();
+            while(true) {
+                draw();
+                KeyStroke key = this.screen.readInput();
+
+                if ((key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') || key.getKeyType() == KeyType.EOF) {
+                    this.screen.close();
+                    break;
+                }
+                processKey(key);
+            }
+
         } catch(IOException e) {
             e.printStackTrace();
         }
