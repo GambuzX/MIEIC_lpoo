bf2d609e913cf700f73cb702b9d279a968357699
GambuzX <mg_mesquita@hotmail.com>
2019-02-25 16:09:43 +0000

    Element abstract class

--- TP02/src/main/java/Hero.java
+++ TP02/src/main/java/Hero.java
@@ -6,45 +6,16 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
 
 
-public class Hero {
+public class Hero extends Element {
 
-    private Position position;
 
     public Hero(int x, int y){
-        position = new Position(x,y);
-    }
-
-    public int getX() {
-        return position.getX();
-    }
-
-    public int getY() {
-        return position.getY();
-    }
-
-    public Position moveUp() {
-        return new Position(position.getX(), position.getY() - 1);
-    }
-
-    public Position moveDown() {
-        return new Position(position.getX(), position.getY() + 1);
-    }
-
-    public Position moveLeft() {
-        return new Position(position.getX() - 1, position.getY());
-    }
-
-    public Position moveRight() {
-        return new Position(position.getX() + 1, position.getY());
+        super(x,y);
     }
 
     public void draw(TextGraphics graphics) {
         graphics.setForegroundColor(TextColor.Factory.fromString("#FFFF33"));
         graphics.enableModifiers(SGR.BOLD);
         graphics.putString(new TerminalPosition(position.getX(), position.getY()), "X");
-    }
-
-    public void setPosition(Position position) {
-        this.position = position;
     }
 }
