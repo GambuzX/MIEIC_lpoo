e2202b8fe0646640f3ea28bad40808b90caec455
Mário Gil <mg_mesquita@hotmail.com>
2019-03-25 15:49:49 +0000

    Observer Pattern

---
+++ TP06/src/main/java/Bar.java
@@ -0,0 +1,36 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Bar {
+    private boolean happyHour = false;
+
+    private List<BarObserver> observers = new ArrayList<BarObserver>();
+
+    public void startHappyHour() {
+        happyHour = true;
+        notifyObservers();
+    }
+
+    public void endHappyHour() {
+        happyHour = false;
+        notifyObservers();
+    }
+
+    public boolean isHappyHour() {
+        return happyHour;
+    }
+
+    void addObserver(BarObserver observer) {
+        observers.add(observer);
+    }
+
+    void removeObserver(BarObserver observer) {
+        observers.remove(observer);
+    }
+
+    void notifyObservers() {
+        for (BarObserver observer : observers)
+            if (isHappyHour()) observer.happyHourStarted(this);
+            else observer.happyHourEnded(this);
+    }
+}
